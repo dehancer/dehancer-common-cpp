f0dee25b8a6933567da9edad757150cefba810c3
Денис Свинарчук <denn.nevera@gmail.com>
2019-10-08 21:20:08 +0300

    cleaned up

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp
-# Build directory: /Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("tests")
