b3351c6b7b0c23216ca7433a77d949b131d84706
Денис Свинарчук <denn.nevera@gmail.com>
2019-10-08 21:08:36 +0300

    cmake fixes

--- cmake-build-debug/lib/cmake_install.cmake
+++ cmake-build-debug/lib/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "/tmp/dehancer")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -30,5 +30,34 @@ endif()
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/cmake-build-debug/lib/libdehancer_common_cpp.a")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libdehancer_common_cpp.a" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libdehancer_common_cpp.a")
+    execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libdehancer_common_cpp.a")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/dehancer" TYPE FILE FILES
+    "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/include/dehancer/Base64.h"
+    "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/include/dehancer/Common.h"
+    "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/include/dehancer/Expected.h"
+    "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/include/dehancer/License.h"
+    "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/include/dehancer/Utils.h"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/dehancer_common_cpp" TYPE FILE FILES
+    "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/cmake-build-debug/lib/generated/dehancer_common_cppConfig.cmake"
+    "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/cmake-build-debug/lib/generated/dehancer_common_cppConfigVersion.cmake"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/dehancer_common_cpp" TYPE FILE FILES "/Users/denn/Develop/Dehancer/Dehancer-Plugins/dehancer-common-cpp/lib/cmake/dehancer_common_cpp.cmake")
 endif()
 
