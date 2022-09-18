49dced989cc3b196158dd649343df4122cc2a1f3
denn nevera m1max 14 <denn.nevera@gmail.com>
2022-09-18 17:26:07 +0300

    controlled singleton add

--- include/dehancer/Common.h
+++ include/dehancer/Common.h
@@ -14,51 +14,51 @@
 #define EXTEND_ENUM(OriginalType, LAST) static_cast<std::underlying_type_t<OriginalType>>(OriginalType::LAST)
 
 namespace dehancer {
-
+    
     using namespace nonstd;
-
+    
     /***
      * Main universal intercommunication structure
      */
     typedef nlohmann::json json;
-
+    
     struct Error;
-
+    
     /***
      * Error handler spec
      */
     using ErrorHandler  = std::function<void(const Error &error)>;
-
+    
     /***
      * Common Error handler
      */
     struct Error {
-
+        
         /**`*
          * Create Error object from error condition or exception message string
          * @param code error condition code
          * @param message exception string
          */
-        Error(const std::error_condition code, const std::string &message = "");
-
+        Error(std::error_condition code, const std::string &message = "");
+        
         /***
          * Get the error value
          * @return code
          */
-        int value() const;
-
+        [[nodiscard]] int value() const;
+        
         /***
          * Get the error message string
          * @return error message
          */
-        std::string message() const;
-
+        [[nodiscard]] std::string message() const;
+        
         /***
          * Error is negative or error can be skipped
          * @return true if error occurred
          */
-        operator bool() const;
-
+        explicit operator bool() const;
+        
         /***
          * Error standard streaming
          * @param os
@@ -66,38 +66,79 @@ namespace dehancer {
          * @return
          */
         friend std::ostream &operator<<(std::ostream &os, const Error &error) {
-            os << error.value() << "/" << error.message();
-            return os;
+          os << error.value() << "/" << error.message();
+          return os;
         }
-
+    
     private:
         std::error_code code_;
         std::string exception_message_;
     };
-
+    
     /***
      * Common singleton interface
      * @tparam T
      */
     template<typename T>
-    class Singleton {
+    class SimpleSingleton {
     public:
         static T &Instance() {
-            static T instance;
-            return instance;
+          static T instance;
+          return instance;
         }
-
+    
     protected:
-        Singleton() {}
-
-        ~Singleton() {}
-
+        SimpleSingleton() = default;
+        ~SimpleSingleton() = default;
+    
     public:
-        Singleton(Singleton const &) = delete;
-
-        Singleton &operator=(Singleton const &) = delete;
+        SimpleSingleton(SimpleSingleton const &) = delete;
+        
+        SimpleSingleton &operator=(SimpleSingleton const &) = delete;
     };
-
+    
+    template<typename T>
+    class ControlledSingleton {
+    public:
+        static T& Instance() {
+          if (!instance)
+          {
+            instance = new T();
+          }
+          return *instance;
+        }
+        static void CreateInstance() {
+          if (!instance)
+          {
+            instance = new T();
+          }
+        }
+        
+        static void DestroyInstance() {
+          if (instance)
+          {
+            delete instance;
+            instance = nullptr;
+          }
+        }
+        static T* instance;
+    
+    protected:
+        ControlledSingleton() = default;
+        ~ControlledSingleton() = default;
+    
+    public:
+        ControlledSingleton(ControlledSingleton const &) = delete;
+        
+        ControlledSingleton &operator=(ControlledSingleton const &) = delete;
+    };
+    
+    #if defined(DEHANCER_CONTROLLED_SINGLETON)
+    template<class T>using Singleton=ControlledSingleton<T>;
+    #else
+    template<class T>using Singleton=SimpleSingleton<T>;
+    #endif
+    
     /***
     *
     * Formated error string
@@ -109,11 +150,11 @@ namespace dehancer {
     std::string error_string(const char *format, ...);
     
     std::string message_string(const char *format, ...);
-
+    
     static inline void _throw_abort(const char *file, int line, const std::string &msg) {
-        std::cerr << "Dehancer logic error: assert failed:\t" << msg << "\n"
-                  << "Dehancer logic error: source:\t\t" << file << ", line " << line << "\n";
-        abort();
+      std::cerr << "Dehancer logic error: assert failed:\t" << msg << "\n"
+                << "Dehancer logic error: source:\t\t" << file << ", line " << line << "\n";
+      abort();
     }
 
 #ifndef NDEBUG
@@ -121,96 +162,96 @@ namespace dehancer {
 #else
 #   define M_Assert(Expr, Msg) ;
 #endif
-
-
+    
+    
     /***
      * Common error codes
      */
     enum class CommonError : int {
-
+        
         /***
          * Skip the error
          */
-                OK = 0,
-
-
+        OK = 0,
+        
         /***
          * not supported error
          */
-                NOT_SUPPORTED = 300,
-
+        NOT_SUPPORTED = 300,
+        
         /***
         *
         */
-                EXCEPTION = 400,
-
+        EXCEPTION = 400,
+        
         /***
          *
          */
-                RESPONSE_ERROR = 403,
-
+        RESPONSE_ERROR = 403,
+        
         /***
          *
          */
-                RPC_ERROR = 500,
-
+        RPC_ERROR = 500,
+        
         /***
          * unknown error
          */
-                UNKNOWN,
-
+        UNKNOWN,
+        
         /***
          * Resource not found
          */
-                NOT_FOUND,
-
+        NOT_FOUND,
+        
         /***
          * Collection range excaption
          */
-                OUT_OF_RANGE,
-
+        OUT_OF_RANGE,
+        
         /**
          * File could not be parsed
          */
-                PARSE_ERROR,
-
+        PARSE_ERROR,
+        
         /**
          * File could not be parsed
          */
-                PERMISSIONS_ERROR,
-
+        PERMISSIONS_ERROR,
+    
+        /**
+         *
+         */
+        FILE_NOT_FOUND,
+    
         /***
          *
          */
-                SECURITY_ISSUE,
-
+        SECURITY_ISSUE,
+        
         /**
          * the last error code
          */
-                LAST
+        LAST
     };
-
+    
     /***
      * Common Error category
      */
     class ErrorCategory : public std::error_category {
     public:
-        virtual const char *name() const noexcept override;
-
-        virtual std::string message(int ev) const override;
-
-        virtual bool equivalent(const std::error_code &code, int condition) const noexcept override;
-
+        [[nodiscard]] const char *name() const noexcept override;
+        [[nodiscard]] std::string message(int ev) const override;
+        [[nodiscard]] bool equivalent(const std::error_code &code, int condition) const noexcept override;
     };
-
+    
     const std::error_category &error_category();
-
+    
     std::error_condition make_error_condition(CommonError e);
-
+  
 }
 
 namespace std {
-
     template<>
     struct is_error_condition_enum<dehancer::CommonError>: public true_type {
     };
